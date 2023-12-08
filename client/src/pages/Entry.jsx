ef2961f76d806fdaec5805c6cc4507bea34b500b
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-12-07 23:47:43 -0600

    set up Entry component background

--- client/src/pages/Entry.jsx
+++ client/src/pages/Entry.jsx
@@ -1,3 +1,22 @@
+import entryBackground from "../assets/img/entryBackground.jpeg"
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 export const Entry = () => {
-    return <>Login</>;
-}
+  const containerStyle = {
+    backgroundImage: `url(${entryBackground})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "white"
+  };
+
+  return (
+    <Box sx={containerStyle}>
+      <Typography variant="h3">temp</Typography>
+    </Box>
+  );
+};
