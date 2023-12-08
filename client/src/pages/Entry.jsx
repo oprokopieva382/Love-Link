e7a7ecb5425a2b8d69a284c3b9cc0b737abaab3d
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-12-08 01:41:08 -0600

    set up form logic and layout and style

--- client/src/pages/Entry.jsx
+++ client/src/pages/Entry.jsx
@@ -1,8 +1,23 @@
-import entryBackground from "../assets/img/entryBackground.jpeg"
+import entryBackground from "../assets/img/entryBackground.jpeg";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import { SignUpFields } from "../components/SignUpFields";
+import { useState } from "react";
 import Typography from "@mui/material/Typography";
+import { FormControlRadio } from "../components/FormControlRadio";
+
 export const Entry = () => {
-  const containerStyle = {
+     const initialFormData = {
+       gender: "",
+       lookingFor: "",
+       firstName: "",
+       lastName: "",
+       password: "",
+     };
+     
+  const [formData, setFormData] = useState(initialFormData);
+
+  const boxStyle = {
     backgroundImage: `url(${entryBackground})`,
     backgroundSize: "cover",
     backgroundPosition: "center",
@@ -10,13 +25,70 @@ export const Entry = () => {
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
-    alignItems: "center",
-    color: "white"
+    alignItems: "end",
+  };
+
+  const formContainerStyle = {
+    marginRight: "30px",
+    display: "flex",
+    flexDirection: "row",
+    gap: 20,
+    color: "white",
+  };
+
+  const handleRadioChange = (field) => (event) => {
+    setFormData({ ...formData, [field]: event.target.value });
+  };
+
+  const handleTextFieldChange = (field) => (event) => {
+    setFormData({ ...formData, [field]: event.target.value });
+  };
+
+  const resetForm = () => {
+    setFormData(initialFormData);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Form Data:", formData);
+    resetForm();
   };
 
   return (
-    <Box sx={containerStyle}>
-      <Typography variant="h3">temp</Typography>
+    <Box sx={boxStyle}>
+      <Typography variant="h3" style={{ margin: "45px" }}>
+        Let's get start
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <Box style={formContainerStyle}>
+          <FormControlRadio
+            title="I am"
+            value={formData.gender}
+            onChange={handleRadioChange("gender")}
+          />
+          <FormControlRadio
+            title="I am looking for"
+            value={formData.lookingFor}
+            onChange={handleRadioChange("lookingFor")}
+          />
+        </Box>
+        <SignUpFields
+          formData={formData}
+          onFirstNameChange={handleTextFieldChange("firstName")}
+          onLastNameChange={handleTextFieldChange("lastName")}
+          onPasswordChange={handleTextFieldChange("password")}
+        />
+        <Box sx={{ textAlign: "center" }}>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            sx={{ padding: "8px 26px" }}
+          >
+            Join
+          </Button>
+        </Box>
+      </form>
     </Box>
   );
 };
