e861e53cf2ed475fb426ea86939e0b6b73fbdc85
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-12-07 18:06:25 -0600

    create routs with RRD

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,21 +3,49 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import App from "./App.jsx";
+import Error from './pages/Error'
+import Entry from "./pages/Entry";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
+import Matches from "./pages/Matches";
+import InTarget from "./pages/InTarget";
+import Conversation from "./pages/Conversation";
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    errorElement: <h1 className="display-2">Wrong page!</h1>,
+    errorElement: <Error />,
     children: [
       {
         index: true,
-        element: < />,
+        element: <Entry />,
       },
       {
-        path: "/saved",
-        element: < />,
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+      {
+        path: "profile/",
+        element: <Profile />,
+      },
+      {
+        path: "matches",
+        element: <Matches />,
+      },
+      {
+        path: "inTarget",
+        element: <InTarget />,
+      },
+      {
+        path: "conversation",
+        element: <Conversation />,
       },
     ],
   },
