243aa3e01bfe5a1564672b5e1ee590830b674dcd
Kurt Riecken <kurt.riecken@gmail.com>
2023-12-08 20:38:34 -0500

    got bones up for conversations page

--- client/src/pages/Conversation.jsx
+++ client/src/pages/Conversation.jsx
@@ -1,5 +1,8 @@
 import testData from '../assets/testData.json';
+import heartIcon from '../assets/img/heart-icon.png'
+import { useState } from "react";
 
+import '../style/conversation.css'
 import { useQuery } from '@apollo/client';
 import { GET_USERS } from '../utils/queries';
 
@@ -13,6 +16,29 @@ export const Conversation = () => {
   //   return <h2>LOADING...</h2>;
   // }
 
+  const [messages, setMessages] = useState([]);
+  const [imageURL, setImageURL] = useState('');
+
+  const mappedData = testData.map(person =>
+    <li key={person.email}>
+      <button className='button' style={{textAlign: "center", margin: "auto"}} onClick={() => getMessages(person._id, person.image, person.outbox)}>
+        <img className="heart-icon" src={heartIcon} alt="Heart Icon" />
+        <img src={person.image} alt="" />
+      </button>
+      <p className='names'>{person.firstName} {person.age}</p>
+    </li>
+  )
+
+  function getMessages(id, image, outbox) {
+    // this will bring back the conversation between the two
+    console.log('click');
+    let newArr = outbox;
+    console.log(newArr[0].text);
+    setMessages(newArr);
+    setImageURL(image)
+  }
+
+
   console.log(testData);
   // console.trace(data);
 
@@ -23,25 +49,34 @@ export const Conversation = () => {
       textAlign: "center",
       padding: "10px"
     }}>
-      <h1 >Dialogues</h1>
+      <h1 >Conversations</h1>
     </div>
     <div style={{
       display: "flex"
-      }}
-      >
+    }}
+    >
       <div style={{
         width: "20%",
         margin: "10px 20px",
         padding: "10px 20px"
       }}>
-        Text2
+        <ul style={{ listStyleType: "none" }}>
+          {mappedData}
+        </ul>
+
       </div>
       <div style={{
         width: "80%",
         margin: "10px 20px",
-        padding: "10px 20px"
+        padding: "10px 20px",
+        backgroundColor: "lightblue"
       }}>
-        text
+        {messages.map(m => 
+          <div key={m.text} style={{display: "flex", flexDirection:"row" }}>
+            <img src={imageURL} alt="" />
+            <p style={{justifyContent: "center"}}>{m.text}</p>
+          </div>)
+        }
       </div>
     </div>
   </>;
